599fc9654ed788550b2d124a96a199c5f52cc6ce
PYnoob2023 <128212032+PYnoob2023@users.noreply.github.com>
2024-06-02 21:52:12 +0900

    Update homework7.py

--- 2024_spring/lec07/homework7.py
+++ 2024_spring/lec07/homework7.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def voiced_excitation(duration, F0, Fs):
     '''
-    Create voiced speeech excitation.
+    Create voiced speech excitation.
     
     @param:
     duration (scalar) - length of the excitation, in samples
@@ -14,8 +14,10 @@ def voiced_excitation(duration, F0, Fs):
       excitation[n] = -1 if n is an integer multiple of int(np.round(Fs/F0))
       excitation[n] = 0 otherwise
     '''
-    excitation = np.zeros(duration) 
-    pass # change this
+    excitation = np.zeros(duration)
+    period = int(np.round(Fs / F0))  # Calculate the period in samples
+    for n in range(0, duration, period):
+        excitation[n] = -1
     return excitation
 
 def resonator(x, F, BW, Fs):
@@ -31,11 +33,22 @@ def resonator(x, F, BW, Fs):
     @returns:
     y (np.ndarray(N)) - resonant output
     '''
-    y = np.zeros(len(x)) 
-    pass # change this
+    N = len(x)
+    y = np.zeros(N)
+    
+    # Calculate the resonator coefficients
+    R = np.exp(-np.pi * BW / Fs)
+    theta = 2 * np.pi * F / Fs
+    a = [1, -2 * R * np.cos(theta), R**2]
+    b = [1 - R]
+    
+    # Apply the resonator filter to the excitation signal
+    for n in range(2, N):
+        y[n] = b[0] * x[n] - a[1] * y[n-1] - a[2] * y[n-2]
+    
     return y
 
-def synthesize_vowel(duration,F0,F1,F2,F3,F4,BW1,BW2,BW3,BW4,Fs):
+def synthesize_vowel(duration, F0, F1, F2, F3, F4, BW1, BW2, BW3, BW4, Fs):
     '''
     Synthesize a vowel.
     
@@ -55,6 +68,10 @@ def synthesize_vowel(duration,F0,F1,F2,F3,F4,BW1,BW2,BW3,BW4,Fs):
     @returns:
     speech (np.ndarray(samples)) - synthesized vowel
     '''
-    speech = np.zeros(duration) # change this
+    excitation = voiced_excitation(duration, F0, Fs)
+    y1 = resonator(excitation, F1, BW1, Fs)
+    y2 = resonator(y1, F2, BW2, Fs)
+    y3 = resonator(y2, F3, BW3, Fs)
+    y4 = resonator(y3, F4, BW4, Fs)
+    speech = y4
     return speech
-    
